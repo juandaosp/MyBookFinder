d2432b7d4215e5a954e8714d31a3802959333222
Juan David Ospina <juandaosp12@gmail.com>
2025-05-25 17:42:06 -0600

    remove console logs

--- src/app/utils/mapBooksData.js
+++ src/app/utils/mapBooksData.js
@@ -1,9 +1,6 @@
 export function mapBoooksData(booksData) {
     return booksData.map(book => {
         const { title, subtitle, authors, description, imageLinks } = book.volumeInfo;
-        console.log('book', book);
-        console.log('imageLinks', imageLinks);
-        console.log('authors', authors);
         return {
             title,
             subtitle,
